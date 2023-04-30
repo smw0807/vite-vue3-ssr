3bea2869db8d7408aa3aed56bf4cb09b2635d405
Minu Song <smw0807@gmail.com>
2023-04-30 14:27:57 +0900

    entry-client 추가

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,1 +1,5 @@
 //앱을 DOM 엘리먼트에 마운트하는 스크립트
+import { createApp } from './main';
+
+const { app, router } = createApp();
+router.isReady().then(() => app.mount('#app'));
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 //환경에 구애받지 않는 (Env-agnostic) 범용 앱 코드로 내보내는 스크립트
-import { createSSRApp } from "vue";
-import { createRouter } from "./router";
-import "./style.css";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import { createRouter } from './router';
+import './style.css';
+import App from './App.vue';
 
 export function createApp() {
   const app = createSSRApp(App);
   const router = createRouter();
   app.use(router);
-  router.isReady().then(() => app.mount("#app"));
   return { app, router };
 }
