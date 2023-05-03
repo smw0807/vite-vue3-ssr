4645c8409d306ba87b9f5839009ed0099c8d8643
Minu Song <smw0807@gmail.com>
2023-05-03 23:31:22 +0900

    pinia 적용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 //환경에 구애받지 않는 (Env-agnostic) 범용 앱 코드로 내보내는 스크립트
 import { createSSRApp } from 'vue';
+import { createPinia } from 'pinia';
 import { createRouter } from './router';
 import './style.css';
 import App from './App.vue';
 
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(createPinia());
   const router = createRouter();
   app.use(router);
   return { app, router };
