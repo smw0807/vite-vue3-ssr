b8e76c3fb6a58b28b04ddb95910ff12fc9d122ee
Minu Song <smw0807@gmail.com>
2023-05-03 23:31:03 +0900

    alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Pages()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
   build: {
     //빌드 시 코드 경량화 여부 옵션
     minify: false,
